1bfcf07f796c7a22b5862a0953182bee0aaa2634
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-01-11 13:03:09 -0600

    Adjusted callback API

--- Sources/Transport/PacketTunnelProvider.swift
+++ Sources/Transport/PacketTunnelProvider.swift
@@ -10,14 +10,8 @@ import Foundation
 import NetworkExtension
 
 extension NEPacketTunnelProvider: PacketTunnelProvider {
-    public func createTCPConnectionThroughTunnel(to remoteEndpoint: NWEndpoint, enableTLS: Bool, tlsParameters TLSParameters: NWTLSParameters?, delegate: Any?, stateCallback: @escaping (NWTCPConnectionState, Error?) -> Void) -> TCPConnection? {
-        let tunnel = createTCPConnectionThroughTunnel(to: remoteEndpoint, enableTLS: enableTLS, tlsParameters: TLSParameters, delegate: delegate)
-        let _ = tunnel.observe(\NWTCPConnection.state) {
-            (conn, change) in
-            
-            stateCallback(change.newValue!, nil)
-        }
-        return tunnel
+    public func createTCPConnectionThroughTunnel(to remoteEndpoint: NWEndpoint, enableTLS: Bool, tlsParameters TLSParameters: NWTLSParameters?, delegate: Any?) -> TCPConnection? {
+        return createTCPConnectionThroughTunnel(to: remoteEndpoint, enableTLS: enableTLS, tlsParameters: TLSParameters, delegate: delegate)
     }
 }
 
@@ -25,6 +19,5 @@ public protocol PacketTunnelProvider {
     func createTCPConnectionThroughTunnel(to remoteEndpoint: NWEndpoint,
                                           enableTLS: Bool,
                                           tlsParameters TLSParameters: NWTLSParameters?,
-                                          delegate: Any?,
-                                          stateCallback: @escaping (NWTCPConnectionState, Error?) -> Void) -> TCPConnection?
+                                          delegate: Any?) -> TCPConnection?
 }
