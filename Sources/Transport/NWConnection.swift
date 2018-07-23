67ac9251dcacebf8779c6f26f4b5acc33da65598
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-07-23 15:19:15 -0500

    Implemeneted NWConnection based on URLSession

--- Sources/Transport/NWConnection.swift
+++ Sources/Transport/NWConnection.swift
@@ -29,10 +29,22 @@ public class NWConnection
     {
         
     }
+
+    var network: URLSessionStreamTask
     
     init(host: NWEndpoint.Host, port: NWEndpoint.Port, using: NWParameters)
     {
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
+        network = session.streamTask(withHostName: "\(host)", port: Int(port.rawValue))
+        network.resume()
         
+        if let viability = viabilityUpdateHandler {
+            viability(true)
+        }
+        
+        if let state = stateUpdateHandler {
+            state(.ready)
+        }
     }
     
     func start(queue: DispatchQueue)
@@ -42,7 +54,10 @@ public class NWConnection
     
     func cancel()
     {
-        
+        if let state = stateUpdateHandler
+        {
+            state(.cancelled)
+        }
     }
     
     var stateUpdateHandler: ((NWConnection.State) -> Void)?
@@ -51,16 +66,69 @@ public class NWConnection
     
     func send(content: Data?, contentContext: NWConnection.ContentContext, isComplete: Bool, completion: NWConnection.SendCompletion)
     {
-        
+        if let data = content
+        {
+            network.write(data, timeout: 0)
+            {
+                (error) in
+                
+                switch completion
+                {
+                    case .contentProcessed(let callback):
+                        if error != nil
+                        {
+                            let nwerr = NWError.posix(POSIXErrorCode.ECONNREFUSED)
+                            callback(nwerr);
+                        }
+                        else
+                        {
+                            callback(nil)
+                        }
+                    case .idempotent:
+                        return
+                }
+            }
+        }
     }
     
     func receive(completion: @escaping (Data?, NWConnection.ContentContext?, Bool, NWError?) -> Void)
     {
-        
+        receive(minimumIncompleteLength: 1, maximumLength: 1024)
+        {
+            (data, context, isComplete, error) in
+            
+            guard error == nil else {
+                completion(nil, context, true, error)
+                return
+            }
+            
+            guard data != nil else {
+                completion(nil, context, false, nil)
+                return
+            }
+            
+            completion(data, context, false, nil)
+        }
     }
     
     func receive(minimumIncompleteLength: Int, maximumLength: Int, completion: @escaping (Data?, NWConnection.ContentContext?, Bool, NWError?) -> Void)
     {
-        
+        network.readData(ofMinLength: minimumIncompleteLength, maxLength: maximumLength, timeout: 60)
+        {
+            (data, bool, error) in
+            
+            guard error == nil else {
+                let nwerr = NWError.posix(POSIXErrorCode.ECONNREFUSED)
+                completion(nil, nil, true, nwerr)
+                return
+            }
+            
+            guard data != nil else {
+                completion(nil, nil, false, nil)
+                return
+            }
+            
+            completion(data, nil, false, nil)
+        }
     }
 }
--- Tests/TransportTests/TransportTests.swift
+++ Tests/TransportTests/TransportTests.swift
@@ -3,10 +3,6 @@ import XCTest
 
 class TransportTests: XCTestCase {
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Transport().text, "Hello, World!")
     }
 
 
