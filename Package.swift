f4519025e88e1beed1cbc48751b3f23e2b162f8e
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-08-20 13:30:20 -0500

    Added SwiftSocket as a dependency to get UDP support

--- Package.swift
+++ Package.swift
@@ -12,6 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "0.1.2"),
+        .package(url: "https://github.com/swiftsocket/SwiftSocket.git", from: "2.0.2"),
     ],
     targets: [
         .target(
