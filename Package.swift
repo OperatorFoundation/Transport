865171ac066f5d6a7f2e0004ce3df61aeedce19c
Lita <looklita@gmail.com>
2018-10-10 17:30:04 -0500

    Dependency Update

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["Transport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "0.1.2")
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "1.0.1")
     ],
     targets: [
         .target(
