371eb6c7b98f6ead24d389875768e61fa0a585ec
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2020-03-31 10:40:32 -0500

    Require macOS v 10.14 or newer

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Transport",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         .library(
             name: "Transport",
