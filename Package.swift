589b4027f565680245e868d4feabeffc66e352c1
consuelita <adelita@operatorfoundation.org>
2020-11-19 18:12:05 -0600

    Specify iOS mimimum deployment target as 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Transport",
     platforms: [
-       .macOS(.v10_15)
+        .macOS(.v10_15),
+        .iOS(.v12)
     ],
     products: [
         .library(
