b5ac00eac45a2e57923540c1fd80c734fe160ffe
consuelita <adelita@operatorfoundation.org>
2021-04-28 19:14:22 -0500

    macOS 11

--- Package.swift
+++ Package.swift
@@ -1,13 +1,14 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
+#if os(macOS)
 let package = Package(
     name: "Transport",
     platforms: [
-        .macOS(.v10_15),
-        .iOS(.v12)
+        .macOS(.v11),
+        .iOS(.v14)
     ],
     products: [
         .library(
@@ -15,7 +16,28 @@ let package = Package(
             targets: ["Transport"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.4"),
+    ],
+    targets: [
+        .target(
+            name: "Transport",
+            dependencies: []),
+        .testTarget(
+            name: "TransportTests",
+            dependencies: ["Transport", "Datable"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+#else
+let package = Package(
+    name: "Transport",
+    products: [
+        .library(
+            name: "Transport",
+            targets: ["Transport"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.0.4"),
         .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.2.3"),
     ],
     targets: [
@@ -28,3 +50,4 @@ let package = Package(
     ],
     swiftLanguageVersions: [.v5]
 )
+#endif
