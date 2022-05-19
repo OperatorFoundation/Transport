0cbd49e3668de97c9e45eb5e464274ee43de871e
Unseelie <emunseelie@gmail.com>
2022-05-19 13:15:59 -0500

    update package platform to include iOS

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -7,6 +7,7 @@ let package = Package(
     name: "Transport",
     platforms: [
         .macOS(.v10_15),
+        .iOS(.v15)
     ],
     products: [
         .library(
