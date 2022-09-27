6b8c0933859dfe6a4a5d93100c387279d12675b2
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2022-09-27 15:06:52 -0500

    Updated macOS version to 12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Transport",
     platforms: [
-        .macOS(.v10_15),
+        .macOS(.v12),
         .iOS(.v15)
     ],
     products: [
